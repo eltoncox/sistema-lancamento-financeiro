c1d1fb0011163f31fbb21332a0958bb0152bb028
eltoncox <eltoncox>
2024-06-19 18:11:28 -0300

    Atualizando Lançamento service, e resource.

--- src/main/java/com/elton/algamoney_api/exceptionHandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/elton/algamoney_api/exceptionHandler/AlgamoneyExceptionHandler.java
@@ -37,8 +37,7 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
-
-		
+	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -46,7 +45,7 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
-		
+	
 	@ExceptionHandler({ EmptyResultDataAccessException.class })
 	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
 		String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
@@ -54,7 +53,7 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
-		
+	
 	@ExceptionHandler({ DataIntegrityViolationException.class } )
 	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
 		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
@@ -93,6 +92,6 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			return mensagemDesenvolvedor;
 		}
 		
-	}    
-    
+	}
+	
 }
--- src/main/java/com/elton/algamoney_api/service/LancamentoService.java
+++ src/main/java/com/elton/algamoney_api/service/LancamentoService.java
@@ -1,15 +1,16 @@
 package com.elton.algamoney_api.service;
 
-import java.util.Optional; // 5.6 Regra para nao salvar pessoa inativa
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import com.elton.algamoney_api.model.Lancamento;
 import com.elton.algamoney_api.model.Pessoa;
 import com.elton.algamoney_api.repository.LancamentoRepository;
 import com.elton.algamoney_api.repository.PessoaRepository;
 import com.elton.algamoney_api.service.exception.PessoaInexistenteOuInativaException;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class LancamentoService {
@@ -28,5 +29,35 @@ public class LancamentoService {
 		
 		return lancamentoRepository.save(lancamento);
 	}
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Optional<Pessoa> pessoa = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		}
+
+		if (pessoa.isEmpty() || pessoa.get().isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+/* 		Optional<Lancamento> lancamentoSalvo = lancamentoRepository.findById(codigo);
+		if (lancamentoSalvo.isEmpty()) {
+			throw new IllegalArgumentException();
+		} */
+		return lancamentoRepository.findById(codigo).orElseThrow(() -> new IllegalArgumentException());
+	}	
 	
 }
